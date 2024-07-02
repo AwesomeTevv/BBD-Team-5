410a73f73ce6c60da5a6383723c63e26550f460e
AwesomeTevv <tevlen.naidoo@gmail.com>
2024-07-02 07:53:23 +0200

    New start

--- server.js
+++ server.js
@@ -1,58 +1,46 @@
 const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
 const app = express();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const server = http.createServer(app);
+const io = socketIo(server);
 
 app.use(express.static("public"));
 
-const rooms = new Map();
+const generateRoomCode = () => {
+  return Math.random().toString(36).substring(2, 8).toUpperCase();
+};
 
 io.on("connection", (socket) => {
+  console.log("a user connected:", socket.id);
+
   socket.on("createRoom", () => {
-    const roomCode = generateRoomCode();
-    rooms.set(roomCode, { host: socket.id, players: [] });
-    socket.join(roomCode);
-    socket.emit("roomCreated", roomCode);
+    const roomId = generateRoomCode();
+    socket.join(roomId);
+    socket.emit("roomCreated", roomId);
+    console.log("Room created:", roomId);
   });
 
-  socket.on("joinRoom", ({ name, roomCode }) => {
-    const room = rooms.get(roomCode);
-    if (room) {
-      room.players.push({ id: socket.id, name });
-      socket.join(roomCode);
-      socket.to(roomCode).emit("playerJoined", { name });
-      io.to(room.host).emit("updatePlayerList", room.players);
-      socket.emit("joinedRoom", { roomCode, isHost: false });
-    } else {
-      socket.emit("error", "Room not found");
-    }
+  socket.on("joinGame", (roomId, playerName) => {
+    socket.join(roomId);
+    io.to(roomId).emit("playerJoined", playerName);
+    console.log(`${playerName} joined room: ${roomId}`);
   });
 
-  socket.on("startGame", (roomCode) => {
-    const room = rooms.get(roomCode);
-    if (room && room.host === socket.id) {
-      io.to(roomCode).emit("gameStarted");
-    }
+  socket.on("startGame", (roomId) => {
+    io.to(roomId).emit("gameStarted");
+  });
+
+  socket.on("gyroscopeData", (roomId, data) => {
+    io.to(roomId).emit("gyroscopeData", data);
   });
 
   socket.on("disconnect", () => {
-    rooms.forEach((room, roomCode) => {
-      const playerIndex = room.players.findIndex((p) => p.id === socket.id);
-      if (playerIndex !== -1) {
-        const player = room.players[playerIndex];
-        room.players.splice(playerIndex, 1);
-        socket.to(roomCode).emit("playerLeft", { name: player.name });
-        io.to(room.host).emit("updatePlayerList", room.players);
-      }
-    });
+    console.log("user disconnected:", socket.id);
   });
 });
 
-function generateRoomCode() {
-  return Math.random().toString(36).substring(2, 6).toUpperCase();
-}
-
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(3000, () => {
+  console.log("listening on *:3000");
 });
